982234843bf7c7e3bdf51a01c17908f0f51554e0
whiteGloom <karabin.m@yandex.ru>
2019-10-21 15:31:06 +0700

    1.1.6 patch
    Список изменений:
    * - добавлена заглушки для фавиконки, ключевых слов, описания;
    * - перекомпановка в папке статики;

--- src/static.js
+++ src/static.js
@@ -4,7 +4,7 @@ function importAll (r) {
 	r.keys().forEach(key => cache[key] = r(key));
 }
 
-importAll(require.context("./static/", true, /\.(jpe?g|gif|png|svg)/));
+importAll(require.context("./static/", true, /\.(jpe?g|gif|png|svg|ico)/));
 importAll(require.context("./static/", true, /\.(woff|otf|ttf|eot)/));
 importAll(require.context("./static/", true, /\.(styl|css)/));
 importAll(require.context("./static/", true, /\.(mp4)/));
